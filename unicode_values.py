2c6c6f966a5370f5663fbec313d6354b3b75fc37
YASHIKA <81742295+Yashikamurugesan@users.noreply.github.com>
2022-07-25 14:32:38 +0530

    Add files via upload

---
+++ unicode_values.py
@@ -0,0 +1,218 @@
+Ayutha_Eluthu={"@u0B83"}
+print("Ayutha_Eluthu")
+for i,j in enumerate(Ayutha_Eluthu):
+    y = j.replace(j[0],'\\')
+    print(i, y)
+
+    
+print("Uyir_Eluthu")
+Uyir_Eluthu={"@u0B85","@u0B86","@u0B87","@u0B88","@u0B89","@u0B8A","@u0B8E","@u0B8F","@u0B90","@u0B92","@u0B93","@u0B94"}
+for i,j in enumerate(Uyir_Eluthu):
+    y = j.replace(j[0],'\\')
+    print(i, y)
+    
+    
+print("Mey_Eluthu")
+Mey_Eluthu={"@u0B95@u0BCD","@u0B99@u0BCD","@u0B9A@u0BCD","@u0B9E@u0BCD",
+        "@u0B9F@u0BCD","@u0BA3@u0BCD","@u0BA4@u0BCD","@u0BA8@u0BCD",
+        "@u0BAA@u0BCD","@u0BAE@u0BCD","@u0BAF@u0BCD","@u0BB0@u0BCD",
+        "@u0BB2@u0BCD","@u0BB5@u0BCD","@u0BB4@u0BCD","@u0BB3@u0BCD",
+        "@u0BB1@u0BCD","@u0BA9@u0BCD"}
+for i,j in enumerate(Mey_Eluthu):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("series")
+series={"@u0B95","@u0B99","@u0B9A","@u0B9E","@u0B9F","@u0BA3","@u0BA4",
+        "@u0BA8","@u0BAA","@u0BAE","@u0BAF","@u0BB0","@u0BB2","@u0BB5",
+        "@u0BB4","@u0BB3","@u0BB1","@u0BA9"}
+for i,j in enumerate(series):
+    y = j.replace(j[0],'\\')
+    print(i, y)
+
+
+
+print("Ka_series")
+kaseries={"@u0B95@u0BBE","@u0B95@u0BBF","@u0B95@u0BC0","@u0B95@u0BC1",
+          "@u0B95@u0BC2","@u0B95@u0BC6","@u0B95@u0BC7","@u0B95@u0BC8",
+          "@u0B95@u0BCA","@u0B95@u0BCB","@u0B95@u0BCC"}
+for i,j in enumerate(kaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+    
+
+print("nya_series")
+nyaseries={"@u0B99@u0BBE","@u0B99@u0BBF","@u0B99@u0BC0",
+"@u0B99@u0BC0","@u0B99@u0BC1","@u0B99@u0BC2","@u0B99@u0BC6",
+"@u0B99@u0BC7","@u0B99@u0BC8","@u0B99@u0BCA",
+"@u0B99@u0BCB","@u0B99@u0BCC"}
+for i,j in enumerate(nyaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("cha_series")
+chaseries={"@u0B9A@u0BBE","@u0B9A@u0BBF","@u0B9A@u0BC0","@u0B9A@u0BC1",
+"@u0B9A@u0BC2","@u0B9A@u0BC6","@u0B9A@u0BC7","@u0B9A@u0BC8",
+"@u0B9A@u0BCA","@u0B9A@u0BCB","@u0B9A@u0BCC"}
+for i,j in enumerate(chaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+    
+print("gnya_series")
+gnyaseries={"@u0B9E@u0BBE","@u0B9E@u0BBF","@u0B9E@u0BC0","@u0B9E@u0BC1",
+            "@u0B9E@u0BC2","@u0B9E@u0BC6","@u0B9E@u0BC7","@u0B9E@u0BC8",
+            "@u0B9E@u0BCA","@u0B9E@u0BCB","@u0B9E@u0BCC"}
+for i,j in enumerate(gnyaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("ta_series")
+taseries ={"@u0B9F@u0BBE","@u0B9F@u0BBF","@u0B9F@u0BC0","@u0B9F@u0BC1",
+            "@u0B9F@u0BC2","@u0B9F@u0BC6","@u0B9F@u0BC7","@u0B9F@u0BC8",
+            "@u0B9F@u0BCA","@u0B9F@u0BCB","@u0B9F@u0BCC"}
+for i,j in enumerate(taseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("naaa_series")    
+naaaseries={"@u0BA3@u0BBE","@u0BA3@u0BBF","@u0BA3@u0BC0","@u0BA3@u0BC1",
+            "@u0BA3@u0BC2","@u0BA3@u0BC6","@u0BA3@u0BC7","@u0BA3@u0BC8",
+            "@u0BA3@u0BCA","@u0BA3@u0BCB","@u0BA3@u0BCC"}
+for i,j in enumerate(naaaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("tha_series")
+thaseries={"@u0BA4@u0BBE","@u0BA4@u0BBF","@u0BA4@u0BC0","@u0BA4@u0BC1",
+            "@u0BA4@u0BC2","@u0BA4@u0BC6","@u0BA4@u0BC7","@u0BA4@u0BC8",
+            "@u0BA4@u0BCA","@u0BA4@u0BCB","@u0BA4@u0BCC"}
+for i,j in enumerate(thaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("nha_series")
+nhaseries={"@u0BA8@u0BBE","@u0BA8@u0BBF","@u0BA8@u0BC0","@u0BA8@u0BC1",
+            "@u0BA8@u0BC2","@u0BA8@u0BC6","@u0BA8@u0BC7","@u0BA8@u0BC8",
+            "@u0BA8@u0BCA","@u0BA8@u0BCB","@u0BA8@u0BCC"}
+
+for i,j in enumerate(nhaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("pa_series")
+paseries={"@u0BAA@u0BBE","@u0BAA@u0BBF","@u0BAA@u0BC0","@u0BAA@u0BC1",
+            "@u0BAA@u0BC2","@u0BAA@u0BC6","@u0BAA@u0BC7","@u0BAA@u0BC8",
+            "@u0BAA@u0BCA","@u0BAA@u0BCB","@u0BAA@u0BCC"}
+for i,j in enumerate(paseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("ma_Series")
+maseries={"@u0BAE@u0BBE","@u0BAE@u0BBF","@u0BAE@u0BC0","@u0BAE@u0BC1",
+            "@u0BAE@u0BC2","@u0BAE@u0BC6","@u0BAE@u0BC7","@u0BAE@u0BC8",
+            "@u0BAE@u0BCA","@u0BAE@u0BCB","@u0BAE@u0BCC"}   
+for i,j in enumerate(maseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("ya_Series")
+yaseries={"@u0BAF@u0BBE","@u0BAF@u0BBF","@u0BAF@u0BC0","@u0BAF@u0BC1",
+            "@u0BAF@u0BC2","@u0BAF@u0BC6","@u0BAF@u0BC7","@u0BAF@u0BC8",
+            "@u0BAF@u0BCA","@u0BAF@u0BCB","@u0BAF@u0BCC"}
+for i,j in enumerate(yaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("ra_Series")
+raseries={"@u0BB0@u0BBE","@u0BB0@u0BBF","@u0BB0@u0BC0","@u0BB0@u0BC1",
+            "@u0BB0@u0BC2","@u0BB0@u0BC6","@u0BB0@u0BC7","@u0BB0@u0BC8",
+            "@u0BB0@u0BCA","@u0BB0@u0BCB","@u0BB0@u0BCC"}
+for i,j in enumerate(raseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("laa_Series")
+laaseries={"@u0BB2@u0BBE","@u0BB2@u0BBF","@u0BB2@u0BC0","@u0BB2@u0BC1",
+            "@u0BB2@u0BC2","@u0BB2@u0BC6","@u0BB2@u0BC7","@u0BB2@u0BC8",
+            "@u0BB2@u0BCA","@u0BB2@u0BCB","@u0BB2@u0BCC"}
+for i,j in enumerate(laaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("va_series")
+vaseries={"@u0BB5@u0BBE","@u0BB5@u0BBF","@u0BB5@u0BC0","@u0BB5@u0BC1",
+            "@u0BB5@u0BC2","@u0BB5@u0BC6","@u0BB5@u0BC7","@u0BB5@u0BC8",
+            "@u0BB5@u0BCA","@u0BB5@u0BCB","@u0BB5@u0BCC"}
+for i,j in enumerate(vaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("sirapulagaram_series")
+sirapulagaramseries={
+            "@u0BB4@u0BBE","@u0BB4@u0BBF","@u0BB4@u0BC0","@u0BB4@u0BC1",
+            "@u0BB4@u0BC2","@u0BB4@u0BC6","@u0BB4@u0BC7","@u0BB4@u0BC8",
+            "@u0BB4@u0BCA","@u0BB4@u0BCB","@u0BA9@u0BCC"}
+for i,j in enumerate(sirapulagaramseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("la_Series")
+laseries={"@u0BB3@u0BBE","@u0BB3@u0BBF","@u0BB3@u0BC0","@u0BB3@u0BC1",
+            "@u0BB3@u0BC2","@u0BB3@u0BC6","@u0BB3@u0BC7","@u0BB3@u0BC8",
+            "@u0BB3@u0BCA","@u0BB3@u0BCB","@u0BA9@u0BCC"}
+for i,j in enumerate(laseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+print("ra_series")
+raseries={"@u0BB1@u0BBE","@u0BB1@u0BBF","@u0BB1@u0BC0","@u0BB1@u0BC1",
+            "@u0BB1@u0BC2","@u0BB1@u0BC6","@u0BB1@u0BC7","@u0BB1@u0BC8",
+            "@u0BB1@u0BCA","@u0BB1@u0BCB","@u0BA9@u0BCC"}
+for i,j in enumerate(raseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("naa_series")
+naaseries={
+            "@u0BA9@u0BBE","@u0BA9@u0BBF","@u0BA9@u0BC0","@u0BA9@u0BC1",
+            "@u0BA9@u0BC2","@u0BA9@u0BC6","@u0BA9@u0BC7","@u0BA9@u0BC8",
+            "@u0BA9@u0BCA","@u0BA9@u0BCB","@u0BA9@u0BCC"}
+for i,j in enumerate(naaseries):
+    y = j.replace(j[0],'\\')
+    y = j.replace(j[6],'\\')
+    print(i, y)
+
+
+print("")    
+
+
+
+
